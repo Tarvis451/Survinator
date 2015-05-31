50f3f94495451c0ef3918149a91b123d22632583
Samuel Roman <Sammyr2011@knights.ucf.edu>
2015-05-31 15:38:29 -0400

    Added ability to register users
    Testing it

---
+++ Survenator Web Page/html/connect.php
@@ -0,0 +1,25 @@
+<?php
+username = "Samuel";
+$password = "P@ssW0rd";
+$hostname = "travis-webserver.dyndns.org"; 
+
+//connection to the database
+$dbhandle = mysql_connect($hostname, $username, $password) 
+ or die("Unable to connect to MySQL");
+echo "Connected to MySQL<br>";
+
+//select a database to work with
+$selected = mysql_select_db("Survinator",$dbhandle) 
+  or die("Could not select Survinator");
+
+//execute the SQL query and return records
+$result = mysql_query("SELECT * FROM Users");
+
+//fetch tha data from the database 
+while ($row = mysql_fetch_array($result)) {
+   echo "UserID:".$row{'UserID'}." UserName:".$row{'UserName'}."HashPassword: ". //display the results
+   $row{'HashPassword'}."<br>";
+}
+//close the connection
+mysql_close($dbhandle);
+?>
