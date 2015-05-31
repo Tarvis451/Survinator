6eeb5b0a623ae2cd6eb759647991b031036d9f99
Tarvis451 <tbangs89@gmail.com>
2015-05-31 17:27:09 -0400

    Update connect.php

--- Survenator Web Page/html/connect.php
+++ Survenator Web Page/html/connect.php
@@ -30,6 +30,7 @@ while ($row = mysql_fetch_array($result)) {
 
 //check if user already exists
 $sqlquery = mysqli_real_escape_string($dbhandle, "SELECT Username FROM Users where Username='{$user}'");
+echo($sqlquery);
 $result = mysql_query($sqlquery);
 
 if(mysql_num_rows($result) == 0)
