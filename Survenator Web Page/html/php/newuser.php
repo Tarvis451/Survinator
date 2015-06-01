ccc028d975c3dc662236b024c7eb7a8c0aceb62f
Tarvis451 <bangstk@knights.ucf.edu>
2015-05-31 20:46:35 -0400

    modified newuser.php

--- Survenator Web Page/html/php/newuser.php
+++ Survenator Web Page/html/php/newuser.php
@@ -2,14 +2,11 @@
 
 $sqlname = "root";
 $sqlpass = "datapass";
-//should be localhost?
-//$hostname = "travis-webserver.dyndns.org"; 
 $hostname = "localhost"; 
 
 //input from registration form
 $user = $_POST['username'];
 $pass = $_POST['password'];
-
 
 //connection to the database
 $dbhandle = mysql_connect($hostname, $sqlname, $sqlpass) 
@@ -19,15 +16,8 @@ echo "Connected to MySQL<br>";
 //select a database to work with
 $selected = mysql_select_db("Survinator",$dbhandle) 
 	or die("Could not select Survinator");
-
-//execute the SQL query and return records
-$result = mysql_query("SELECT * FROM Users");
-
-//fetch tha data from the database 
-while ($row = mysql_fetch_array($result)) {
-	echo "UserID:".$row{'UserID'}."<br>UserName:".$row{'UserName'}."<br>HashPassword: ". //display the results
-	$row{'HashPassword'}."<br>";
-}
+	
+//TODO - prevent invalid usernames (e.g. empty strings)
 
 //check if user already exists
 //FIXME - protect query from injection!!
@@ -38,16 +28,21 @@ $result = mysql_query($sqlquery);
 if(mysql_num_rows($result) != 0)
 {
 	mysql_close($dbhandle);
+	echo "<script language=\"JavaScript\">\n";
+	echo "alert('Username already taken!');\n";
+	echo "window.location='/UserRegistration.html'";
+	echo "</script>";
 	die("User already exists!");
 }
 
 //make new user
+//TODO - get hashed password instead of plaintext
 $sqlquery = "INSERT INTO Users(Username, HashPassword) VALUES ('{$user}', '{$pass}')";
 $result = mysql_query($sqlquery);
-echo($result);
 
-//redirect to login page, print confirm message
+//redirect to registration success page
 mysql_close($dbhandle);
+header("location:../UserRegistrationSuccess.html");
 die("You are registered!");
 
 ?>
