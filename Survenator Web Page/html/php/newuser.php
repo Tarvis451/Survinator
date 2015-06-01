8e573b508218f3270e13ba570f533727585e2d3e
Tarvis451 <bangstk@knights.ucf.edu>
2015-05-31 20:14:20 -0400

    Registration: Update php

--- Survenator Web Page/html/php/newuser.php
+++ Survenator Web Page/html/php/newuser.php
@@ -1,4 +1,5 @@
 <?php
+
 $sqlname = "root";
 $sqlpass = "datapass";
 //should be localhost?
@@ -12,20 +13,20 @@ $pass = $_POST['password'];
 
 //connection to the database
 $dbhandle = mysql_connect($hostname, $sqlname, $sqlpass) 
- or die("Unable to connect to MySQL");
+	or die("Unable to connect to MySQL");
 echo "Connected to MySQL<br>";
 
 //select a database to work with
 $selected = mysql_select_db("Survinator",$dbhandle) 
-  or die("Could not select Survinator");
+	or die("Could not select Survinator");
 
 //execute the SQL query and return records
 $result = mysql_query("SELECT * FROM Users");
 
 //fetch tha data from the database 
 while ($row = mysql_fetch_array($result)) {
-   echo "UserID:".$row{'UserID'}."<br>UserName:".$row{'UserName'}."<br>HashPassword: ". //display the results
-   $row{'HashPassword'}."<br>";
+	echo "UserID:".$row{'UserID'}."<br>UserName:".$row{'UserName'}."<br>HashPassword: ". //display the results
+	$row{'HashPassword'}."<br>";
 }
 
 //check if user already exists
@@ -33,18 +34,20 @@ while ($row = mysql_fetch_array($result)) {
 $sqlquery = "SELECT Username FROM Users where Username='{$user}'";
 $result = mysql_query($sqlquery);
 
+//fail if user already exists
 if(mysql_num_rows($result) != 0)
+{
+	mysql_close($dbhandle);
     die("User already exists!");
-
-else {
-  $sqlquery = "INSERT INTO Users(Username, HashPassword) VALUES ('{$user}', '{$pass}')";
-  $result = mysql_query($sqlquery);
-  echo($result);
 }
 
-
 //make new user
+$sqlquery = "INSERT INTO Users(Username, HashPassword) VALUES ('{$user}', '{$pass}')";
+$result = mysql_query($sqlquery);
+echo($result);
 
-//close the connection
+//redirect to login page, print confirm message
 mysql_close($dbhandle);
+die("You are registered!");
+
 ?>
