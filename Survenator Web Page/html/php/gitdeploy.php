034adde2964edc21e50401c8a151c10cf5a8bc9d
Tarvis451 <bangstk@knights.ucf.edu>
2015-05-31 20:17:20 -0400

    Deploy: Fixed missing "

--- Survenator Web Page/html/php/gitdeploy.php
+++ Survenator Web Page/html/php/gitdeploy.php
@@ -17,9 +17,9 @@ if ( isset($_POST['payload']) )
 	shell_exec("cd {$REPO_PULL_LOC} && git pull");
 	
 	//Copy web folder contents to actual web folder on server
-	shell_exec("cd {$REPO_PULL_LOC}/{$WEB_FOLDER_NAME} && rsync -r --delete . {$COPY_LOC});
+	shell_exec("cd {$REPO_PULL_LOC}/{$WEB_FOLDER_NAME} && rsync -r --delete . {$COPY_LOC}");
 
-	exit("Recieved payload and updated server");
+	exit("Received payload and updated server");
 }
 else
 	exit("Rejected; no payload");
