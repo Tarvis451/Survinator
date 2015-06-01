0bc5df72d956aecaf0c422da80fa80020cd43277
Tarvis451 <tbangs89@gmail.com>
2015-06-01 17:46:29 -0400

    Initial login.php

--- Survenator Web Page/html/php/login.php
+++ Survenator Web Page/html/php/login.php
@@ -1,7 +1,44 @@
 <?php
 
-    $user = $_POST['username'];
-    $pass = $_POST['password'];
+    require 'dbconnect.php';
+    $dbhandle = db_connect();
 
-    header("location: /MainOptions.html");
+    $user = mysql_real_escape_string($_POST['username']);
+    $pass = mysql_real_escape_string($_POST['password']);
+    
+    //query user
+    $sqlquery = "SELECT Username FROM Users where Username='{$user}'";
+    $result = mysql_query($sqlquery);
+    
+    //fail if user already exists
+    if(mysql_num_rows($result) == 0)
+    {
+	    mysql_close($dbhandle);
+	    die("Specified user does not exist<br>");
+    }
+    
+    //query password
+    //TODO - use hashed password instead of plaintext
+    $sqlquery = "SELECT UserID FROM Users WHERE Username='{$user}' AND HashPassword='{$pass}' LIMIT 1";
+    $result = mysql_query($sqlquery);
+    
+    //fail if wrong password
+    if(mysql_num_rows($result) == 0)
+    {
+	    mysql_close($dbhandle);
+	    die("Incorrect password<br>");
+    }
+    
+    $row = mysqli_fetch_array($result);
+    
+    //create login session
+    session_start();
+    $_SESSION["userid"] = $row['UserID'];
+    $_SESSION["user"] = $user;
+    
+    db_close($dbhandle);
+    
+    //redirect to main page
+    header("Location: /MainOptions.html");
+    
 ?>
