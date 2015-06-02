ccfc5d16167a67f2bbe09e60890a55a6144f13f6
Tarvis451 <bangstk@knights.ucf.edu>
2015-06-02 18:32:02 -0400

    Fixed description block

--- Survenator Web Page/html/php/form_CreateSurvey.php
+++ Survenator Web Page/html/php/form_CreateSurvey.php
@@ -4,7 +4,7 @@
  
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
 	Survey Name: <input type="text" name="Name" value="">
-	Survey Description: <textarea name="Description" value="" rows="5" cols="40">
+	Survey Description: <textarea name="Description" value="" rows="5" cols="40"></textarea>
 	<input type="submit" name="submit" value="Add Questions">
 	<br><br><input type="button" name="cancel" value="Cancel">
 </form>
