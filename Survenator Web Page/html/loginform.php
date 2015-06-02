5a89d28cf4e709ae8ae6ba6cc4bec1d6e21ff83a
Tarvis451 <tbangs89@gmail.com>
2015-06-02 15:23:48 -0400

    Create php login form instead of using html

---
+++ Survenator Web Page/html/loginform.php
@@ -0,0 +1,46 @@
+//Dynamic form for login, included in html
+//Actual login info is a separate script
+
+<?php
+
+require '/php/login.php';
+
+//handle sent form, call real login function, do stuff based on return
+$error = "";
+$userfield = "";
+
+if(isset($_POST['submit']))
+{
+	$user = mysql_real_escape_string($_POST['username']);
+	$pass = mysql_real_escape_string($_POST['password']);
+	
+	$res = login($user, $pass);
+	
+	//successful login
+	if ($res == 0)
+	{
+		$return = "Logged in as {$user}!<br>";
+		$url = "/MainOptions.html";
+		
+    	header("Location: ".$url);
+    	die($return);
+    }
+    
+	else if ($res == 91)
+		$error = "The specified user does not exist.";
+	else if ($res == 92)
+		$error = "Incorrect password.";
+		
+	$userfield = $user;
+	
+}
+
+?>
+
+<form action= <?php echo $_SERVER["PHP_SELF"]; ?> method="post">
+      Username: <input type="text" name="username" value=<?php echo $fieldname; ?> >
+      Password: <input type="password" name="password" value="">
+      <input type="submit" value="Log In">
+</form>
+
+?>
