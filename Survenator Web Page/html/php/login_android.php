0618ac187161a363d505dd404acb4f18421abb88
Tarvis451 <tbangs89@gmail.com>
2015-06-02 17:09:28 -0400

    Update to use external login function

--- Survenator Web Page/html/php/login_android.php
+++ Survenator Web Page/html/php/login_android.php
@@ -1,53 +1,37 @@
 <?php
-    require 'dbconnect.php';
-    $dbhandle = db_connect();
-    $user = mysql_real_escape_string($_POST['username']);
-    $pass = mysql_real_escape_string($_POST['password']);
+    require 'login.php';
     
-    //query user
-    $sqlquery = "SELECT UserName FROM Users where UserName='{$user}'";
-    $result = mysql_query($sqlquery);
+    $user = $_POST['username'];
+    $pass = $_POST['password'];
     
-    //fail if user already exists
-    if(mysql_num_rows($result) == 0)
+    $ret = login($user, $pass);
+    
+    //fail if user does not exist
+    if ($ret == 91)
     {
-	    mysql_close($dbhandle);
 	    $response["error"] = 91;
 	    $response["message"] = "Specified user does not exist";
             echo json_encode($response);
 	    die();
     }
     
-    //query password
-    //TODO - use hashed password instead of plaintext
-    $sqlquery = "SELECT UserID FROM Users WHERE UserName='{$user}' AND HashPassword='{$pass}' LIMIT 1";
-    $result = mysql_query($sqlquery);
-    
     //fail if wrong password
-    if(mysql_num_rows($result) == 0)
+    if($ret == 92)
     {
-	    mysql_close($dbhandle);
 	    $response["error"] = 92;
             $response["message"] = "Incorrect password";
             echo json_encode($response);
 	    die();
     }
     
-    $row = mysql_fetch_array($result);
-    
-    //create login session
-    session_start();
-    $_SESSION["userid"] = $row['UserID'];
-    $_SESSION["user"] = $user;
-    
-    db_close($dbhandle);
-    
-    //redirect to main page
-    //header("Location: /MainOptions.html");
-    $response["error"] = 0;
-    $response["message"] = "Logged in";
-    $response["userid"] = $_SESSION["userid"];
-    echo json_encode($response);
-    die();
+    //success
+    if($ret == 0)
+    {
+    	$response["error"] = 0;
+    	$response["message"] = "Logged in";
+    	$response["userid"] = $_SESSION["userid"];
+    	echo json_encode($response);
+    	die();
+    }
     
 ?>
