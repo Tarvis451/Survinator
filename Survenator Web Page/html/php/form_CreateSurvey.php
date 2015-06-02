250300020d42e89490ca7b1facefddddd5e89405
Tarvis451 <tbangs89@gmail.com>
2015-06-02 19:19:43 -0400

    Update form_CreateSurvey.php

--- Survenator Web Page/html/php/form_CreateSurvey.php
+++ Survenator Web Page/html/php/form_CreateSurvey.php
@@ -1,10 +1,52 @@
- <?php
+<?php
+	include 'login.php';
+	include 'addSurvey.php';
 	
- ?>
+	$error = "";
+	$fieldname = "";
+	$fielddesc = "";
+	
+	if (!checkLogin())
+	{
+		header("Location: /index.html");
+		die("You must log in to create surveys");
+	}
+	
+	if (isset($_POST['cancel']))
+	{
+		header("Location: /mainOptions.html");
+		die("Canceled adding survey");
+	}
+	
+	if (isset($_POST['submit']))
+	{
+		$name = $_POST['title'];
+		$desc = $_POST['description'];
+		
+		$fieldname = $name;
+		$fielddesc = $desc;
+		
+		$ret = addSurvey($name,$desc);
+		
+		if ($ret == -101)
+			$error = "Survey title must have 3 or more characters";
+		
+		if ($ret == -102)
+			$error = "Survey title already taken";
+		
+		if ($ret <= 0)
+		{
+			$surveyid = $ret;
+			header("Location: /addQuestions?id={$surveyid}");
+			die("Successfully created survey with id {$surveyid}");
+		}
+	}
+?>
  
+<center><text color='red'><?php echo $error; ?></text></center>
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-	Survey Name: <input type="text" name="Name" value="">
-	<center>Survey Description: <textarea name="Description" value="" rows="5" cols="40"></textarea></center>
+	Survey Title: <input type="text" name="title" value="<?php echo $fieldname; ?>">
+	<center>Survey Description: <textarea name="description" value="<?php echo $fielddesc; ?>" rows="5" cols="40"></textarea></center>
 	<input type="submit" name="submit" value="Add Questions">
 	<br><br><input type="submit" name="cancel" value="Cancel">
 </form>
