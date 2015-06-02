34d302c944ef2492924cfc6410b224b4dc66f180
Tarvis451 <bangstk@knights.ucf.edu>
2015-06-02 18:34:02 -0400

    Try to center description box

--- Survenator Web Page/html/php/form_CreateSurvey.php
+++ Survenator Web Page/html/php/form_CreateSurvey.php
@@ -1,10 +1,10 @@
  <?php
- 
+	
  ?>
  
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
 	Survey Name: <input type="text" name="Name" value="">
-	Survey Description: <textarea name="Description" value="" rows="5" cols="40"></textarea>
+	<center>Survey Description: <textarea name="Description" value="" rows="5" cols="40"></textarea></center>
 	<input type="submit" name="submit" value="Add Questions">
-	<br><br><input type="button" name="cancel" value="Cancel">
+	<br><br><input type="submit" name="cancel" value="Cancel">
 </form>
