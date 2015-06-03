7f898a2664d54a7cbba57388e3c5a4a36e42dc15
Tarvis451 <tbangs89@gmail.com>
2015-06-02 22:08:59 -0400

    Update getUnpublishedSurveys.php

--- Survenator Web Page/html/php/getUnpublishedSurveys.php
+++ Survenator Web Page/html/php/getUnpublishedSurveys.php
@@ -3,17 +3,17 @@
 //Returns table of surveys that belong to you and are unpublished.
 //This is so you can leave and finish an unpublished survey later.
 
-function getUnpublishedSurveys($inuserid)
+function getUnpublishedSurveys()
 {
 	//Supplied userid doesn't match your login.
-	if ($_SESSION['userid'] != $inuserid)
-		return -1;
+	//if ($_SESSION['userid'] != $inuserid)
+	//	return -1;
 	
 	include 'dbconnect.php';
 	$dbhandle = db_connect();
 	
 	//get DB query
-	$userid = mysql_real_escape_string($inuserid);
+	$userid = mysql_real_escape_string($_SESSION['userid']);
 	
 	$query = "SELECT SurveyID, SurveyName from SurveyList WHERE UserID = {$userid} AND Published = 0";
 	$result = mysql_query($query);
@@ -30,3 +30,17 @@ function getUnpublishedSurveys($inuserid)
 }
 
 ?>
+
+<table style="width:100%">
+	<tr>
+		<th>Unpublished Surveys</th>
+	</tr>
+	<tr>
+<?php
+$surveylist = getUnpublishedSurveys();
+foreach ($surveylist as $survey)
+{
+	echo <td><a href = "/create.html?id={$survey['SurveyID']}">$survey['SurveyName']</a></td>;
+}
+  	</tr>
+</table> 
