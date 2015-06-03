d52cc10aa5ac75031da8a6fb344cddd221531515
Tarvis451 <tbangs89@gmail.com>
2015-06-02 22:50:19 -0400

    Update getPublishedSurveys.php

--- Survenator Web Page/html/php/getPublishedSurveys.php
+++ Survenator Web Page/html/php/getPublishedSurveys.php
@@ -28,19 +28,3 @@ function getPublishedSurveys()
 }
 
 ?>
-<?php 
-
-$surveylist = getPublishedSurveys();
-
-if ($surveylist != 0)
-{?>
-<table style="width:100%">
-	<tr>
-		<th>Select Survey</th>
-	</tr>
-<?php
-foreach ($surveylist as $survey)
-	echo "<tr><td><center><a href = '/surveyOptions.html?id={$survey['SurveyID']}'>{$survey['SurveyName']}</a></center></td></tr>";
-?>
-</table> 
-<?php } ?>
