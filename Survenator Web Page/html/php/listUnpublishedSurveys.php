af8e8ca805e5879114c3a69f9eb7398751464a5b
Tarvis451 <tbangs89@gmail.com>
2015-06-02 22:50:06 -0400

    Create listUnpublishedSurveys.php

---
+++ Survenator Web Page/html/php/listUnpublishedSurveys.php
@@ -0,0 +1,23 @@
+<?php 
+include_once 'getUnpublishedSurveys.php';
+
+$surveylist = getUnpublishedSurveys();
+
+if ($surveylist > 0)
+{
+?>
+
+<table style="width:100%">
+	<tr>
+		<th>Your Unpublished Surveys</th>
+	</tr>
+<?php
+foreach ($surveylist as $survey)
+	echo "<tr><td><center><a href = '/create.html?id={$survey['SurveyID']}'>{$survey['SurveyName']}</a></center></td></tr>";
+?>
+</table> 
+
+<?php 
+} 
+
+?>
