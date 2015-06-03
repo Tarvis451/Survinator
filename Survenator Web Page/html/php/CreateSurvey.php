8ec46c977861463feca93f43e53403a7f69709c1
Tarvis451 <tbangs89@gmail.com>
2015-06-02 22:57:46 -0400

    Update CreateSurvey.php

--- Survenator Web Page/html/php/CreateSurvey.php
+++ Survenator Web Page/html/php/CreateSurvey.php
@@ -1,3 +1,47 @@
 <?php
 
+function addSurvey($intitle, $indesc)
+{
+	require 'dbconnect.php';
+
+	$dbhandle = db_connect();
+
+	$title = mysql_real_escape_string($intitle);
+	$desc = mysql_real_escape_string($indesc);
+	$userid = $_SESSION['userid'];
+
+	//check if name is valid
+	if(strlen($title) < 3)
+	{
+		db_close($dbhandle);
+		return -101;
+	}
+
+	//check if survey name is taken
+	$query = "SELECT SurveyName FROM SurveyList WHERE SurveyName = '{$title}'";
+	$result = mysql_query($query);
+
+	if(mysql_num_rows($result) != 0)
+	{
+		db_close($dbhandle);
+		return -102;
+	}
+
+	//info looks good, add to db
+	$query = "INSERT INTO SurveyList(SurveyName, UserID, Published, SurveyDescription) VALUES ('{$title}','{$userid}',0,'{$desc}')";
+	$result = mysql_query($query);
+
+	//get survey id for retrn
+	$query = "SELECT SurveyID FROM SurveyList WHERE SurveyName = '{$title}'";
+	$result = mysql_query($query);
+
+	$row = mysql_fetch_array($result);
+
+	$surveyid = $row['SurveyID'];
+
+	db_close($dbhandle);
+
+	return($surveyid);	
+}
+
 ?>
