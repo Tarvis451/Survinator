20c6e03b07e30d586dc9798f3d860f3d92411462
Tarvis451 <tbangs89@gmail.com>
2015-06-04 19:08:15 -0400

    Update form_AddTF.php

--- Survenator Web Page/html/php/form_AddTF.php
+++ Survenator Web Page/html/php/form_AddTF.php
@@ -2,14 +2,14 @@
 	//include_once 'login.php';
 	include_once 'addQuestion.php';
 	
-	$SurveyID = $_GET['id'];
+	$surveyid = $_GET['id'];
 	
 	$error = "";
 	$fieldtext = "";
 	
 	if (isset($_POST['cancel']))
 	{
-		header("Location: /create.html?id={$SurveyId}");
+		header("Location: /create.html?id={$surveyid}");
 		die("Canceled adding question");
 	}
 	
@@ -19,25 +19,33 @@
 		
 		$fieldtext = $text;
 		
-		$ret = addQuestionTF($text,$SurveyID);
+		$ret = addQuestionTF($text,$surveyid);
+		if ($ret == -1)
+			$error = "Survey id#{$surveyid} does not belong to you!";
 		
 		if ($ret == -201)
 			$error = "Question title must have 3 or more characters";
 		
 		if ($ret == -202)
 			$error = "Question title already taken";
+			
+		if ($ret == -203)
+			$error = "Survey id#{$surveyid} does not exist!";
+			
+		if ($ret == -204)
+			$error = "Database error";
 		
 		if ($ret >= 0)
 		{
-			header("Location: /create.html?id={$SurveyId}");
-			die("Successfully added a new question to Survey #{$SurveyId}");
+			header("Location: /create.html?id={$surveyid}");
+			die("Successfully added a new question to Survey #{$surveyid}");
 		}
 	}
 ?>
  
 <center><font color='red'><?php echo $error; ?></font></center>
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-	Question Text: <input type="text" name="text" value="<?php echo $fieldname; ?>">
+	Question Text: <input type="text" name="text" value="<?php echo $fieldtext; ?>">
 	<input type="submit" name="submit" value="Add Question">
 	<br><br><input type="submit" name="cancel" value="Cancel">
 </form>
