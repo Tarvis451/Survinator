18ff903bf7577326e61010e0b19b07c4b4a36d3f
Tarvis451 <tbangs89@gmail.com>
2015-06-04 19:19:49 -0400

    Update addQuestion.php

--- Survenator Web Page/html/php/addQuestion.php
+++ Survenator Web Page/html/php/addQuestion.php
@@ -10,24 +10,39 @@ function addQuestionTF($intext, $insid)
 	$text = mysql_real_escape_string($intext);
 	$sid = mysql_real_escape_string($insid);
 	
-	//check if user is logged in
-	if (!isset($_SESSION['userid']))
+	//check if requested survey to add to actually exists
+	$query = "SELECT * FROM SurveyList WHERE SurveyID={$sid}";
+	$result = mysql_query($query);
+	
+	if(mysql_num_rows($result) == 0)
+	{
+		db_close($dbhandle);
+		return -203;
+	}
+	
+	//check if this survey belongs to user adding question
+	$uid = getUserID();
+	
+	$query = "SELECT UserID FROM SurveyList WHERE SurveyID={$sid}";
+	$result = mysql_query($query);
+	
+	$row = mysql_fetch_array($result);
+	
+	if($row['UserID'] != $uid)
 	{
 		db_close($dbhandle);
 		return -1;
 	}
-	
-	//$userid = $_SESSION['userid'];
 
-	//check if name is valid
+	//check if question text is valid
 	if(strlen($text) < 3)
 	{
 		db_close($dbhandle);
 		return -201;
 	}
 
-	//check if text is already used
-	$query = "SELECT DISTINCT QuestionText FROM Surveys WHERE SurveyID={$sid}";
+	//check if question text is already used
+	$query = "SELECT QuestionText FROM Surveys WHERE SurveyID={$sid}";
 	$result = mysql_query($query);
 
 	if(mysql_num_rows($result) != 0)
@@ -39,10 +54,12 @@ function addQuestionTF($intext, $insid)
 	$qid = getNewQuestionID($sid);
 
 	//info looks good, add to db
+	//Add TRUE response
 	$query = "INSERT INTO Surveys(SurveyID, QuestionID, QuestionType, QuestionText, ResponseID, ResponseText) VALUES ({$sid}, {$qid}, 'TF', '{$text}', 1, 'A: True')";
 	$result = mysql_query($query);
 	
-	$query = "INSERT INTO Surveys(SurveyID, QuestionID, QuestionType, QuestionText, ResponseID,R esponseText) VALUES ({$sid}, {$qid}, 'TF', '{$text}', 2, 'B: False')";
+	//Add FALSE response
+	$query = "INSERT INTO Surveys(SurveyID, QuestionID, QuestionType, QuestionText, ResponseID, ResponseText) VALUES ({$sid}, {$qid}, 'TF', '{$text}', 2, 'B: False')";
 	$result = mysql_query($query);
 
 	db_close($dbhandle);
@@ -55,10 +72,15 @@ function getNewQuestionID($insid)
 	$query = "SELECT QuestionID FROM Surveys WHERE SurveyID='{$insid}' ORDER BY 'QuestionID' DESC LIMIT 1";
 	$result = mysql_query($query);
 	
+	//no questions added yet, this will be  the first
+	if(mysql_num_rows($result) == 0)
+		return 1;
+	
+	//add 1 to highest existing qid
 	$row = mysql_fetch_array($result);
 
 	$qid = $row['QuestionID'];
-	$qid += 1;
+	$qid++;
 	
 	return $qid;
 }
