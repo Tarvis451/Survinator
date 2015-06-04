f3a0a683dc940f4426c9cd7f9d5f6d6dfcbe3d11
Travis <bangstk@knights.ucf.edu>
2015-06-04 16:54:12 -0400

    Neater question list

--- Survenator Web Page/html/php/listQuestionsAdd.php
+++ Survenator Web Page/html/php/listQuestionsAdd.php
@@ -6,15 +6,25 @@ if ($questionlist != 0)
 {
 
 ?>
-
+<center><b>Survey Questions</b></center>
 <table style="width:100%">
 	<tr>
-		<th>Survey Questions:</th>
+		
+		<th>Question #</th>
+		<th>Question Text</th>
+		<th>Question Type</th>
 	</tr>
 	
 <?php
 foreach ($questionlist as $question)
-	echo "<tr><td><center><a href = '/editQuestion.html?sid={$question['SurveyID']}&qid={$question['QuestionID']}'>Question {$question['QuestionID']}: {$question['QuestionText']} ({$question['QuestionType']})</a></center></td></tr>";
+	echo "<tr><td>{$question['QuestionID'}</td></tr>";
+	echo "<tr><td><center><a href = '/editQuestion.html?sid={$id}&qid={$question['QuestionID']}'>{$question['QuestionText']})</a></center></td></tr>";
+	if ($question['QuestionType'] == "TF")
+		echo "<tr><td>True/False</td></tr>";
+	if ($question['QuestionType'] == "MC")
+		echo "<tr><td>Multiple Choice</td></tr>";
+	if ($question['QuestionType'] == "FR")
+		echo "<tr><td>Free Response</td></tr>";
 ?>
 
 </table> 
