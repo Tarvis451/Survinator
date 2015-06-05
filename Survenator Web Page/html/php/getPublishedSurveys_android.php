12c1a5319a4d5e52c0452d4442f689cee166a239
Tarvis451 <tbangs89@gmail.com>
2015-06-04 20:58:39 -0400

    Test simpler getPublishedSurveys
    Andrew, feel free to revert this if it doesn't work

--- Survenator Web Page/html/php/getPublishedSurveys_android.php
+++ Survenator Web Page/html/php/getPublishedSurveys_android.php
@@ -1,28 +1,9 @@
 <?php
 //Returns table of surveys that are published
 //When you click one you can take the survey or view results, or delete it if it is your survey.
-
-	include_once 'dbconnect.php';
-	$dbhandle = db_connect();
-
-	//get DB query
-	$query = "SELECT SurveyID, SurveyName from SurveyList WHERE Published = 1";
-	$result = mysql_query($query);
-
-	if (mysql_num_rows($result) == 0)
-	{
-		die();
-	}
-
-	//build the array to send out with json
-	$surveylist = array();
-
-	while ($row = mysql_fetch_array($result))
-	{
-		$surveylist[] = array( "SurveyID" => $row["SurveyID"], "SurveyName" => $row["SurveyName"]);
-	}
- 
-	db_close($dbhandle);
+	require_once 'getPublishedSurveys.php';
+	
+	$surveylist = getPublishedSurveys();
 
 	echo json_encode($surveylist);
 	die();
