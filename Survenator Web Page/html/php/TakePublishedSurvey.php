a3c95b8ee87f9b53f8fce1ef667fcae02720116b
Samuel Roman <Sammyr2011@knights.ucf.edu>
2015-06-06 19:04:33 -0400

    Updated create.html and PollView.html and Created TakePublishedSurvey.php
    Should allow you to see your poll with questions and methods to answer
    those questions after you click on a published survey.

---
+++ Survenator Web Page/html/php/TakePublishedSurvey.php
@@ -0,0 +1,57 @@
+<?php 
+include_once 'getQuestions.php';
+$surveyid = $_GET['surveyid'];
+$questionlist = getQuestions($surveyid);
+
+if ($questionlist != 0)
+{
+
+?>
+<center><b>Survey Questions</b></center>
+<br><br>
+<table style="width:100%; text-align:center" >
+	<tr>
+		<th>Question #</th>
+		<th>Question Text</th>
+		<th>Question Type</th>
+	</tr>
+	
+<?php
+//formatting will be weird
+//maybe use lists instead?
+//or new line before and after question input
+    
+    
+    
+    
+foreach ($questionlist as $question)
+{
+	echo "<tr><td>{$question['QuestionID']}</td>";
+	echo "<td><a href = '/editQuestion.html?surveyid={$surveyid}&questionid={$question['QuestionID']}'>{$question['QuestionText']}</a></td>";
+	
+    //Add radio button
+    if ($question['QuestionType'] == "TF")
+		echo 
+        "<td>
+            <input type="radio" name="TF"
+            <?php if (isset($TF) && $TF=="T") echo                         "checked";?>
+                value="T">True
+                <input type="radio" name="TF"
+                <?php if (isset($TF) && $TF=="F") echo                       "checked";?>        
+                value="F">False
+        </td>";
+	if ($question['QuestionType'] == "MC")
+		echo "<td>Multiple Choice</td>";
+	if ($question['QuestionType'] == "SA")
+		echo "<td><textarea rows="4" cols="40"></textarea></td>";
+	echo "</tr>";
+}
+?>
+
+</table> 
+
+<?php 
+
+} 
+
+?>
