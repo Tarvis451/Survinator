92d02eb02a4452a2669ca005de489042fbd096e6
Tarvis451 <tbangs89@gmail.com>
2015-06-07 21:42:55 -0400

    Update getQuestions.php

--- Survenator Web Page/html/php/getQuestions.php
+++ Survenator Web Page/html/php/getQuestions.php
@@ -1,7 +1,6 @@
 <?php
 
 //Returns table of questions
-
 function getQuestions($surveyid)
 {
 	include_once 'dbconnect.php';
@@ -14,16 +13,40 @@ function getQuestions($surveyid)
 	if (mysql_num_rows($result) == 0)
 		return 0;
 	
+	//Turn SQL result into array for returning
 	$questionlist = array();
 	
 	while ($row = mysql_fetch_array($result))
-	{
 		array_push($questionlist, $row);
-	}
 	
 	db_close($dbhandle);
 	
 	return $questionlist;
+}
+
+//Returns table of responses for T/F or MC questions
+function getResponses($surveyid, $questionid)
+{
+	include_once 'dbconnect.php';
+	$dbhandle = db_connect();
+	
+	//get DB query
+	$query = "SELECT ResponseID, ResponseText from Surveys WHERE SurveyID={$surveyid} AND QuestionID={$questionid}";
+	$result = mysql_query($query);
+	
+	//Question has no answers!!
+	if (mysql_num_rows($result) == 0)
+		return 0;
+	
+	//Turn SQL result into array for returning
+	$responselist = array();
+	
+	while ($row = mysql_fetch_array($result))
+		array_push($responselist, $row);
+	
+	db_close($dbhandle);
+	
+	return $responselist;
 }
 
 ?>
