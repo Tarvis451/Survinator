2c5561194e9deee320b433d26397bcceba6433a7
Tarvis451 <bangstk@knights.ucf.edu>
2015-06-11 20:37:02 -0400

    Clean up results page

--- Survenator Web Page/html/php/ShowBarGraphs.php
+++ Survenator Web Page/html/php/ShowBarGraphs.php
@@ -1,124 +1,51 @@
-<?php 
-include_once 'getQuestions.php';
-$surveyid = $_GET['surveyid'];
-$questionlist = getQuestions($surveyid);
+<?php
 
-if ($questionlist != 0)
-{
+require_once 'getQuestions.php';
+require_once 'getResponses.php';
+
+$surveyid = $_GET['surveyid'];
+$questionid = $_GET['questionid'];
+
+$questionlist = getQuestions($surveyid);
+$question = $questionlist['$questionid'];
+$questiontext = $question['QuestionTitle'];
+$questiontype = $question['QuestionType'];
 
 ?>
-<style>
-table, th, td {
-	border: 1px solid black;
-}
-</style>
 
-<br><br>
-<table id="poll" style="width:100%; text-align:center;" >
+<b><center>Question <?php echo $questionid; ?>: </b><?php echo $questiontext; ?></center>
+
+<?php
+
+if (questiontype = "SA")
+{
+	$results = getResponseList($surveyid, $questionid); ?>
+	
+	<table id="poll" style="width:100%; text-align:center;" >
 	<tr>
-		<th>Question #</th>
-		<th>Question Text</th>
+		<th>Responses</th>
+	</tr>
+	
+	<?php
+	foreach ($responses as $response);
+		echo "<tr><td>{$results['Response']}</td></tr>";
+}
+else
+{
+	$results = getResponseChoices($surveyid, $questionid); ?>
+	
+	<table id="poll" style="width:100%; text-align:center;" >
+	<tr>
+		<th>Response Text</th>
 		<th>Results</th>
 	</tr>
 	
-<?php
-foreach ($questionlist as $question)
-{
-	echo "<tr><td>{$question['QuestionID']}</td>";
-	echo "<td>{$question['QuestionText']}</td>";
-	
-	//Query number of yes' and no's
-	if ($question['QuestionType'] == "TF") {
-		//each query counts the number of true and false questions there are
-		$queryT = "SELECT COUNT(*) AS CountT FROM Responses WHERE SurveyID={$surveyid} AND QuestionID={$question['QuestionID']} AND Response=1";
-		$queryF = "SELECT COUNT(*) AS CountF FROM Responses WHERE SurveyID={$surveyid} AND QuestionID={$question['QuestionID']} AND Response=2";
-		
-		?>
-		<!--
-		<td>
-		True:    <img src="poll.gif"
-				width='<?php echo(100*round($queryT/($queryF+$queryT),2)); ?>' height='20'>
-				<?php echo(100*round($queryT/($queryF+$queryT),2)); ?>%
-				<br>
-				
-		False:     <img src="poll.gif"
-				width='<?php echo(100*round($queryF/($queryF+$queryT),2)); ?>' height='20'>
-				<?php echo(100*round($queryF/($queryF+$queryT),2)); ?>%
-		
-		</td>
-		-->
-		
-		<td style="text-align:left">
-		True:    <img src="poll.gif" width='81' height='20'>
-				<br>
-				
-		False:     <img src="poll.gif" width='19' height='20'>
-		</td>
-		
-		<?php
+	<?php
+	foreach ($responses as $response)
+	{
+		echo "<tr><td>{$response['ResponseText']}</td>";
+		echo "<td>{$response['Count']}</td><?tr>";
 	}
-		
-	
-	//Query number using response 1 with this question on this survey
-	if ($question['QuestionType'] == "MC") {
-		//Each query counts the number of users that voted for each response.
-		
-		// This query produces one single integer in the column "CountA".
-		$query1 = "SELECT COUNT(*) AS CountA FROM Responses WHERE SurveyID={$surveyid} AND QuestionID={$question['QuestionID']} AND Response=1";
-		
-		// This query produces one single integer in the column "CountB".
-		$query2 = "SELECT COUNT(*) AS CountB FROM Responses WHERE SurveyID={$surveyid} AND QuestionID={$question['QuestionID']} AND Response=2";
-
-		// This query produces one single integer in the column "CountC".
-		$query3 = "SELECT COUNT(*) AS CountC FROM Responses WHERE SurveyID={$surveyid} AND QuestionID={$question['QuestionID']} AND Response=3";
-
-		// This query produces one single integer in the column "CountD".
-		$query4 = "SELECT COUNT(*) AS CountD FROM Responses WHERE SurveyID={$surveyid} AND QuestionID={$question['QuestionID']} AND Response=4";
-		
-		?>
-		
-		<td>
-		A:    <img src="poll.gif"
-				width='<?php echo(100*round($query1/( $query1 + $query2 + $query3 + $query4 ),2)); ?>' height='20'>
-				<?php echo(100*round($queryT/($queryF+$queryT),2)); ?>%
-				<br>
-				
-		B:     <img src="poll.gif"
-				width='<?php echo(100*round($query2/( $query1 + $query2 + $query3 + $query4 ),2)); ?>' height='20'>
-				<?php echo(100*round($queryF/($queryF+$queryT),2)); ?>%
-				<br>
-				
-		C:     <img src="poll.gif"
-				width='<?php echo(100*round($query3/( $query1 + $query2 + $query3 + $query4 ),2)); ?>' height='20'>
-				<?php echo(100*round($queryF/($queryF+$queryT),2)); ?>%
-				<br>
-		
-		D:     <img src="poll.gif"
-				width='<?php echo(100*round($query4/( $query1 + $query2 + $query3 + $query4 ),2)); ?>' height='20'>
-				<?php echo(100*round($queryF/($queryF+$queryT),2)); ?>%
-		</td>
-		
-		<?php
-		
-	}
-	
-	//list 4 most recent responses
-	if ($question['QuestionType'] == "SA") {
-		
-		// This query returns each short answer response to this question.
-		$query = "SELECT Response FROM Responses WHERE SurveyID={$surveyid} AND QuestionID={$question['QuestionID']}";
-	}
-	
-	
-	
-	echo "</tr>";
 }
-?>
-
-</table> 
-
-<?php 
-
-} 
-
+	
 ?>
--- Survenator Web Page/html/php/getResponses.php
+++ Survenator Web Page/html/php/getResponses.php
@@ -4,22 +4,42 @@ require_once 'dbconnect.php';
 require_once 'login.php';
 
 //For MC and TF
-function getResponseChoices($surveyid, $questionid, $numChoices)
+function getResponseChoices($surveyid, $questionid)
 {
 	$dbhandle = db_connect();
 	
 	$surveyid = mysql_real_escape_string($surveyid);
 	$questionid = mysql_real_escape_string($questionid);
 	
+	//get number of choices
+	$query = "SELECT * FROM Surveys WHERE SurveyID={$surveyid} AND QuestionID={$questionid}";
+	$result = mysql_query($query);
+	
+	$numChoices = mysql_num_rows($result);
+	
+	//form the array of response counts
 	$responses = array();
 	
 	for ($response = 1; $response <= $numChoices; $response++)
 	{
-		$query = "SELECT COUNT(*) AS Count FROM Responses WHERE SurveyID={$surveyid} AND QuestionID={$questionid} AND Response='{$response}'";
-		$result = mysql_query($query);
+		$responserow = array();
 		
-		$row = mysql_fetch_array($result);
-		array_push($responses, $row['Count']);
+		//get response text
+		$query = "SELECT ResponseText FROM Surveys WHERE SurveyID={$surveyid} AND QuestionID={$questionid} AND ResponseID={$response}";
+		$result_text = mysql_query($query);
+		
+		$row = mysql_fetch_array($result_text);
+		array_push($responserow, $row['ResponseText']);
+		
+		//get response count
+		$query = "SELECT COUNT(*) AS Count FROM Responses WHERE SurveyID={$surveyid} AND QuestionID={$questionid} AND Response='{$response}'";
+		$result_count = mysql_query($query);
+		
+		$row = mysql_fetch_array($result_count);
+		array_push($responserow, $row['Count']);
+		
+		//add both to output array
+		array_push($responses, $responserow);
 	}
 	
 	db_close($dbhande);
