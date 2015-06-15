5001cd5b0a0c9ddc8e1157f4eaeec9ac4b540c07
Tarvis451 <tbangs89@gmail.com>
2015-06-15 11:39:07 -0400

    Update form_AddMC_aux.php

--- Survenator Web Page/html/php/form_AddMC_aux.php
+++ Survenator Web Page/html/php/form_AddMC_aux.php
@@ -6,12 +6,10 @@
  * Time: 9:39 PM
  */
  
- 	//Get the Survey ID
-	$surveyid = $_GET['surveyid'];
  
 ?>
 
-<form action="<?php echo $_SERVER['form_AddMC.php']; ?>" method="post">
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
     
     <br><input type="submit" name="submit" value="Finish">
     <input type="hidden" name="surveyid" value="<?php echo $surveyid; ?>">
